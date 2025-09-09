24d71b2efe03586f25651555b722d468adfdfcbe
skillbridgeconnectofficial <skillbridgeconnect.official@gmail.com>
2025-09-09 07:21:05 +0530

    Update4 signup.js

--- signup.js
+++ signup.js
@@ -15,7 +15,7 @@ const form = document.getElementById('signup-form');
       }
 
       try {
-        const res = await fetch("http://localhost:5000/signup", {
+        const res = await fetch("https://skillbridge-connect.onrender.com", {
           method: "POST",
           headers: { "Content-Type": "application/json" },
           body: JSON.stringify({ username, password })
@@ -34,4 +34,5 @@ const form = document.getElementById('signup-form');
         errorMsg.textContent = "⚠️ Server error";
         successMsg.textContent = "";
       }
+
     });
